34717542c487bf629034b596431bf6649a54ced4
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-24 23:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const Promise1 = () => { return new Promise((resolve, reject) => {
+  const inp = parseInt(document.querySelector('#ip').value);
+  setTimeout(()=>{
+    resolve(inp)
+  },2000)
+})
+}
+
+const Promise2 = () => { return new Promise((resolve, reject) => {
+  const inp = parseInt(document.querySelector('#ip').value);
+  setTimeout(()=>{
+    resolve(inp*2)
+  },2000)
+})}
+
+const Promise3 = () => { return new Promise((resolve,reject)=>{
+  const inp = parseInt(document.querySelector('#ip').value);
+  setTimeout(()=>{
+    resolve(inp*2-3)
+  },1000)
+})}
+
+
+
+document.querySelector('#btn').addEventListener('click',()=>{
+  Promise1().then((num1) => {
+    document.querySelector('#output').innerHTML = `Result: ${num1}`;
+    return Promise2();
+  }).then((num2) => {
+    document.querySelector('#output').innerHTML = `Result: ${num2}`;
+    return Promise3();
+  }).then((num3)=>{
+    document.querySelector('#output').innerHTML = `Result: ${num3}`;
+  }).catch((err)=>{
+    document.querySelector('#output').innerHTML = `Error: ${err}`;
+  })
+})
